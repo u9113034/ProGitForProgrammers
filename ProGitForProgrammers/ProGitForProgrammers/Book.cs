5f4e9034b2e49a2520295752d4105e1958f79ec6
Chou Kung Mao <u9113034@gmail.com>
2024-02-28 00:44:39 +0800

    add book

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProGitForProgrammers
+{
+    class Book
+    {
+        public string Title { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+        
+    }
+}
+

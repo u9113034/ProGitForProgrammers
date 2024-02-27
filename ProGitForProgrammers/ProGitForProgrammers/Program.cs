9d19975d525ab378948b2628e1eedde4febc5d2c
Chou Kung Mao <u9113034@gmail.com>
2024-02-27 23:53:17 +0800

    my message

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
+Console.WriteLine("I just added this in the command line repo");
+Console.WriteLine("This line add to show the staging area");
